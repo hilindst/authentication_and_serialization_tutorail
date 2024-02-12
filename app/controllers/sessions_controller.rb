d96f1be2d8751a7cc8512589c1ee3340fd877b14
Hilary Lindstrom <hilindst@gmail.com>
2024-02-12 14:52:02 -0600

    Authorization and Authentication rails

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.find_by(username: params[:username])
+    if user&.authenticate(params[:password])
+      render json: { token: '123' }
+    else
+      render json: { error: 'Invalid username or password' }, status: :unauthorized
+    end
+  end
+
+  private 
+  
+  def jwt_encode(payload, exp = 24.hours.from_now)
+    payload[:exp] = exp.to_i
+    JWT.encode(payload, Rails.application.secrets.secret_key_base)
+  end
+end
+
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  before_action :authenticate_request, except: [:create]
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: :created
+    else
+      render json: user.errors, status: :unprocessable_entity
+    end
+  end
+ 
+  def show 
+    user = User.find_by(id: params[:id])
+
+    render json: UserBlueprint.render(user, view: :normal), status: :ok
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :password, :password_confirmation)
+  end
+end
+
